e0e5df8fef3af3619671944d26c52252e87ea886
gayeon00 <chlrkdus4819@gmail.com>
2023-07-19 09:23:40 +0900

    feat: splashscreen

--- Mini01_LBS01/app/src/main/java/com/test/mini01_lbs01/MainActivity.kt
+++ Mini01_LBS01/app/src/main/java/com/test/mini01_lbs01/MainActivity.kt
@@ -2,10 +2,16 @@ package com.test.mini01_lbs01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.test.mini01_lbs01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
