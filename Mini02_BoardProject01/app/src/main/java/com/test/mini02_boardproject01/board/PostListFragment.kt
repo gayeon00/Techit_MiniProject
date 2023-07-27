88ecdf9e434fe71c0021f440f149b6956ac35848
gayeon00 <chlrkdus4819@gmail.com>
2023-07-27 14:50:45 +0900

    feat: reclycerview
    recyclerview 장착

--- Mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/board/PostListFragment.kt
+++ Mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/board/PostListFragment.kt
@@ -1,12 +1,16 @@
 package com.test.mini02_boardproject01.board
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.test.mini02_boardproject01.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.LayoutParams
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.test.mini02_boardproject01.databinding.FragmentPostListBinding
+import com.test.mini02_boardproject01.databinding.RowPostListBinding
 
 class PostListFragment : Fragment() {
     lateinit var fragmentPostListBinding: FragmentPostListBinding
@@ -18,6 +22,43 @@ class PostListFragment : Fragment() {
         fragmentPostListBinding = FragmentPostListBinding.inflate(layoutInflater)
         boardMainActivity = activity as BoardMainActivity
         // Inflate the layout for this fragment
+
+        fragmentPostListBinding.run {
+            recyclerViewAll.run {
+                adapter = AllRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
         return fragmentPostListBinding.root
+    }
+
+    inner class AllRecyclerViewAdapter : Adapter<AllRecyclerViewAdapter.AllViewHolder>() {
+        inner class AllViewHolder(rowPostListBinding: RowPostListBinding) :
+            ViewHolder(rowPostListBinding.root) {
+            val rowPostListSubject = rowPostListBinding.rowPostListSubject
+            val rowPostListNickName = rowPostListBinding.rowPostListNickName
+
+            init {
+
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AllViewHolder {
+            val rowPostListBinding = RowPostListBinding.inflate(layoutInflater)
+            rowPostListBinding.root.layoutParams = ViewGroup.LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.WRAP_CONTENT
+            )
+            return AllViewHolder(rowPostListBinding)
+        }
+
+        override fun getItemCount(): Int {
+            return 100
+        }
+
+        override fun onBindViewHolder(holder: AllViewHolder, position: Int) {
+            holder.rowPostListSubject.text = "제목입니다: $position"
+            holder.rowPostListNickName.text = "작성자 : $position"
+        }
     }
 }
