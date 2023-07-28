cac4cf0d2250a85145c2be0bf5b2a19d9cca2735
gayeon00 <chlrkdus4819@gmail.com>
2023-07-28 15:18:50 +0900

    feat: appbar 수정 직전

--- Mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/board/PostReadFragment.kt
+++ Mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/board/PostReadFragment.kt
@@ -1,11 +1,12 @@
 package com.test.mini02_boardproject01.board
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.test.mini02_boardproject01.R
 import com.test.mini02_boardproject01.databinding.FragmentPostReadBinding
@@ -22,13 +23,22 @@ class PostReadFragment : Fragment() {
         val actionBar = (activity as AppCompatActivity).supportActionBar
         actionBar?.hide()
 
+// 뒤로가기 버튼 이벤트 처리
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+
+            // 뒤로가기 버튼을 눌렀을 때 항상 'a' 프래그먼트로 이동
+            findNavController().navigate(R.id.action_postReadFragment_to_postListFragment)
+
+        }
+
 
         fragmentPostReadBinding.run {
             materialToolbar2.setOnMenuItemClickListener {
-                when(it.itemId) {
+                when (it.itemId) {
                     R.id.item_edit -> {
                         findNavController().navigate(R.id.action_postReadFragment_to_postModifyFragment)
                     }
+
                     R.id.item_delete -> {
                         findNavController().navigate(R.id.action_postReadFragment_to_postListFragment)
                     }
@@ -37,10 +47,11 @@ class PostReadFragment : Fragment() {
             }
 
             materialToolbar2.setNavigationOnClickListener {
-                findNavController().popBackStack()
+                findNavController().navigate(R.id.action_postReadFragment_to_postListFragment)
             }
         }
         return fragmentPostReadBinding.root
     }
+
 
 }
--- Mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/board/PostWriteFragment.kt
+++ Mini02_BoardProject01/app/src/main/java/com/test/mini02_boardproject01/board/PostWriteFragment.kt
@@ -7,6 +7,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.test.mini02_boardproject01.R
@@ -22,34 +23,29 @@ class PostWriteFragment : Fragment() {
         fragmentPostWriteBinding = FragmentPostWriteBinding.inflate(layoutInflater)
         boardMainActivity = activity as BoardMainActivity
 
-        setHasOptionsMenu(true)
+        val actionBar = (activity as AppCompatActivity).supportActionBar
+        actionBar?.hide()
 
+        fragmentPostWriteBinding.run {
+            materialToolbar4.setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.item_camera -> {
+                    }
+                    R.id.item_gallery -> {
+                    }
+                    R.id.item_complete -> {
+                        findNavController().navigate(R.id.action_postWriteFragment_to_postReadFragment)
+                    }
+                }
+                true
+            }
+
+            materialToolbar4.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
         // Inflate the layout for this fragment
         return fragmentPostWriteBinding.root
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.write, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        //누른 거에 따라 작업 다르게 해주기
-        return when (item.itemId) {
-            R.id.item_camera -> {
-                true
-            }
-
-            R.id.item_gallery -> {
-                true
-            }
-
-            R.id.item_complete -> {
-                findNavController().navigate(R.id.action_postWriteFragment_to_postReadFragment)
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
