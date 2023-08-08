4d15cad6f9b7b7c309566367c4fb23627f48e3fd
gayeon00 <chlrkdus4819@gmail.com>
2023-08-09 00:49:35 +0900

    feat: 로그아웃, 회원탈퇴 제거 및 NavigationUI로 이전
    - BoardActivity navigation UI로 이전

--- Mini02_BoardProject03/app/src/main/java/com/test/mini02_boardproject03/board/BoardActivity.kt
+++ Mini02_BoardProject03/app/src/main/java/com/test/mini02_boardproject03/board/BoardActivity.kt
@@ -1,18 +1,25 @@
 package com.test.mini02_boardproject03.board
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.test.mini02_boardproject03.R
 import com.test.mini02_boardproject03.databinding.ActivityBoardBinding
-import com.test.mini02_boardproject03.user.MainActivity
 
 class BoardActivity : AppCompatActivity() {
     lateinit var activityBoardBinding: ActivityBoardBinding
+    lateinit var drawerLayout: DrawerLayout
+
+    //nav에 따라 앱바 타이틀을 바꿔주고, navigationIcon의 역할과 모양을 바꿔주는, drawerlayout도 보여주는 놈!
+    lateinit var appBarConfiguration: AppBarConfiguration
+
+    //navHost에 있는 앱 네비게이션을 관리해주는 놈
     lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,60 +27,42 @@ class BoardActivity : AppCompatActivity() {
 
         activityBoardBinding = ActivityBoardBinding.inflate(layoutInflater)
         setContentView(activityBoardBinding.root)
-
         setSupportActionBar(activityBoardBinding.boardToolbar)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.menu_24px)
+        drawerLayout = activityBoardBinding.drawerLayout
 
-        activityBoardBinding.navigationView.setNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.postListFragment,
-                R.id.postFreeFragment,
-                R.id.postHumorFragment,
-                R.id.postSportsFragment,
-                R.id.postQnaFragment,
-                R.id.modifyUserFragment -> {
-
-                    navController.navigate(it.itemId)
-                }
-
-                R.id.item_logout,
-                R.id.item_delete -> {
-                    goToMainActivity()
-                }
-            }
-
-            activityBoardBinding.drawerLayout.closeDrawer(GravityCompat.START)
-            true
-        }
-
+        //navController
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
         navController = navHostFragment.navController
-    }
-
-    private fun goToMainActivity() {
-        val newIntent = Intent(this, MainActivity::class.java)
-        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(newIntent)
-        finish()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> {
-                activityBoardBinding.drawerLayout.openDrawer(GravityCompat.START)
-            }
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            // 네비게이션 이벤트 발생 시 실행될 코드
+            activityBoardBinding.navigationView.setCheckedItem(destination.id)
         }
-        return super.onOptionsItemSelected(item)
+        //navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.postListFragment,
+                R.id.postQnaFragment,
+                R.id.postHumorFragment,
+                R.id.postHumorFragment,
+                R.id.postFreeFragment
+            ), drawerLayout
+        )
+
+
+        //navController Setup
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        activityBoardBinding.navigationView.setupWithNavController(navController)
+        activityBoardBinding.navigationView.setCheckedItem(R.id.postListFragment)
+        //navController Setup
+
     }
 
-    override fun onBackPressed() { //뒤로가기 했을 때
-        if (activityBoardBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            activityBoardBinding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+    //뒤로가기 버튼을 누르면 호출되는 함수!
+    override fun onSupportNavigateUp(): Boolean {
+        //최상위가 아니라 뒤로갈 수 있으면 이 줄이 실행됨
+        return navController.navigateUp(appBarConfiguration)
     }
 }
